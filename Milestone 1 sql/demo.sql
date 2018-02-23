e6e94ea7787ea76e943f1dd9208a545647fe4586
souidan <ahmed.souidan@student.guc.edu.eg>
2018-02-24 00:11:37 +0200

    modified .gitignore soudian

---
+++ Milestone 1 sql/demo.sql
@@ -0,0 +1,15 @@
+Select * from  "signup"(false,'wafa@gmail.com','wafaa_shanta','98765','Egypt','wafaa','shanta');
+Select * from  "blockuser"(1,3);
+Select * from "edituser"(1,true,'m','England','mohamed','ahmed');
+Select * from "likephoto"(1,1);
+Select * from "dislikephoto"(1,2);
+Select * from "unlikephoto"(1,1);
+Select * from "undislikephoto"(1,2);
+Select * from "followcategories"(ARRAY[1,2],2);
+Select * from "followuser"(2,3);
+Select * from "followuser"(1,20);
+Select * from "unfollowuser"(2,3);
+Select * from  "unblockuser"(1,3);
+
+
+
