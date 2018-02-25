6c0e22b925b59220eb341e22ce5539cf77bb2335
Kefa7y <midomoka94@hotmail.com>
2018-02-25 17:38:05 +0200

    Post collection in post application is almost done + .gitignore modified

--- post_application/src/main/java/ArangoInstance.java
+++ post_application/src/main/java/ArangoInstance.java
@@ -1,26 +1,22 @@
-import com.arangodb.ArangoCursor;
-import com.arangodb.ArangoDB;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
+import com.arangodb.ArangoDB;
+import com.arangodb.entity.DocumentCreateEntity;
+import com.arangodb.ArangoDBException;
 
 public class ArangoInstance {
 
 
-    ArangoDB arangoDB;
+    public ArangoDB arangoDB;
 
 
     public ArangoInstance(String user, String password){
         String rootPath = "src/main/resources/";
-
-        try {
-            InputStream in = new FileInputStream(rootPath + "arango.properties");
-        }catch (FileNotFoundException e){
-            System.err.println("arongo properties file not found "+e.getMessage());
-        }
+//
+//        try {
+//            InputStream in = new FileInputStream(rootPath + "arango.properties");
+//        }catch (FileNotFoundException e){
+//            System.err.println("arongo properties file not found "+e.getMessage());
+//        }
 
         arangoDB = new ArangoDB.Builder().user(user).password(password).build();
     }
@@ -28,67 +24,52 @@ public class ArangoInstance {
 
     public void initializeDB(){
 
-        String dbName = "Post";
-        arangoDB.createDatabase("Post");
-        arangoDB.db(dbName).createCollection("posts");
-        arangoDB.db(dbName).createCollection("comments");
-        arangoDB.db(dbName).createCollection("categories");
+
+        try{
+            String dbName = "Post";
+            arangoDB.createDatabase("Post");
+            arangoDB.db(dbName).createCollection("posts");
+            arangoDB.db(dbName).createCollection("comments");
+            arangoDB.db(dbName).createCollection("categories");
+            System.out.println("Database created: " + dbName);
+        } catch (ArangoDBException e) {
+            System.err.println("Failed to create database: Post");
+        }
     }
 
     public void dropDB(){
 
-        arangoDB.db("Post").drop();
+        try{
+            arangoDB.db("Post").drop();
+            System.out.println("Database dropped: Post");
+        } catch (ArangoDBException e) {
+            System.err.println("Failed to drop database: Post");
+        }
     }
 
     public void insertNewPost(PostDBObject postDBObject){
         arangoDB.db("Post").collection("posts").insertDocument(postDBObject);
     }
 
-    public PostDBObject getPost(String uuid1,String uuid2){
-        //type of hashmap is sketchy
-        Map<String, Object> bindVars = new HashMap<String,Object>();
-//        bindVars.put("uuid1",uuid1);
-//        bindVars.put("uuid2",uuid2);
-//        String query= "FOR chat IN chats FILTER chat.uuid1 == @uuid1 && chat.uuid2 == @uuid2  RETURN chat";
-        ArangoCursor<PostDBObject> cursor = arangoDB.db("Post").query(query,bindVars, null, PostDBObject.class);
-
-        while(cursor.hasNext()) {
-            PostDBObject post = cursor.next();
-            return post;
-        }
-
-        bindVars.clear();
-//        bindVars.put("uuid1",uuid2);
-//        bindVars.put("uuid2",uuid1);
-//        query= "FOR chat IN chats FILTER chat.uuid1 == @uuid1 && chat.uuid2 == @uuid2  RETURN chat";
-        cursor = arangoDB.db("Post").query(query,bindVars, null, PostDBObject.class);
-
-        for(; cursor.hasNext-+();) {
-            PostDBObject post = cursor.next();
-            return post;
-        }
-        return null;
+    public PostDBObject getPost(String id){
+        PostDBObject post =arangoDB.db("Post").collection("posts").getDocument(id, PostDBObject.class);
+        return post;
     }
 
-//    public void sendMessage(String message,String from,String to){
-//        //getChat to getPost
-//        PostDBObject chat = getChat(from,to);
-//        if(chat == null){
-//            chat = new ChatDBObject(from,to);
-//            insertNewPost(post);
-//        }
-//    }
+
 
 
     public static void main(String[] args){
-        ArangoInstance arangoInstance = new ArangoInstance("root","122195");
-       // arangoInstance.initializeChatsDB();
-        //arangoInstance.dropChatsDB();
-        //PostDBObject postDBObject = new PostDBObject("Zaher","Souidan");
+        ArangoInstance arango = new ArangoInstance("root","pass");
+//        arango.initializeDB();
 
-       // arangoInstance.arangoDB.db("Chat").collection("chats").insertDocument(chatDBObject);
-        arangoInstance.sendMessage("Hi","Ahmed","Souidan");
-        //System.out.println(arangoInstance.getChat("Zaher","Souidan").getUuid1());
+//        PostDBObject post = new PostDBObject();
+//        post.setUser_id("mama");
+//        arango.insertNewPost(post);
+
+        PostDBObject post =arango.getPost("757");
+
+        System.out.println(post);
     }
 
 
