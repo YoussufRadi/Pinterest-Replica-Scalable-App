fc79dfdb0421c2426e9644b1e99b03f2d189c829
Youssuf Radi <y.sameh1995@gmail.com>
2018-04-16 02:18:47 +0200

    Commands Post Modified

--- Service Server/src/main/java/PostCommands/UpdatePost.java
+++ Service Server/src/main/java/PostCommands/UpdatePost.java
@@ -3,58 +3,13 @@ package PostCommands;
 
 import Database.ArangoInstance;
 import Interface.ConcreteCommand;
-import Models.Message;
 import Models.PostDBObject;
 import Models.PostLiveObject;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Envelope;
 import org.json.JSONObject;
 import org.redisson.api.RLiveObjectService;
 
-import java.io.IOException;
-import java.util.HashMap;
-
 
 public class UpdatePost extends ConcreteCommand {
-
-    @Override
-    protected void execute() {
-        HashMap<String, Object> parameters = data;
-
-
-        Channel channel = (Channel) parameters.get("channel");
-
-        RLiveObjectService RLiveObjectService = (RLiveObjectService)
-                parameters.get("RLiveObjectService");
-        ArangoInstance ArangoInstance = (ArangoInstance)
-                parameters.get("ArangoInstance");
-
-        AMQP.BasicProperties properties = (AMQP.BasicProperties) parameters.get("properties");
-        AMQP.BasicProperties replyProps = (AMQP.BasicProperties) parameters.get("replyProps");
-        Envelope envelope = (Envelope) parameters.get("envelope");
-        JsonParser jsonParser = new JsonParser();
-        System.out.println(properties.getReplyTo());
-
-        JsonObject jsonObject = (JsonObject) jsonParser.parse((String) parameters.get("body"));
-        Gson gson = new GsonBuilder().create();
-        Message message = gson.fromJson((String) jsonObject.get("body").toString(), Message.class);
-
-
-
-        try {
-            channel.basicPublish("", properties.getReplyTo(), replyProps, jsonObject.toString().getBytes("UTF-8"));
-            channel.basicAck(envelope.getDeliveryTag(), false);
-            //System.out.println(envelope.getDeliveryTag());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
 
     @Override
     protected void doCommand() {
